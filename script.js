0914823beab197dd0f0de56b66ce55d1c3c603d2
CoolestKitKatCoder <tinycodingcatmeow@proton.me>
2026-03-19 07:23:26 +0400

    Update script.js

--- script.js
+++ script.js
@@ -5,3 +5,15 @@ function showCat() {
   document.getElementById("catText").style.display = "block";
   document.getElementById("catImg").style.display = "block";
 }
+
+function showCat() {
+  let text = document.getElementById("catText");
+  let img = document.getElementById("catImg");
+
+  text.style.display = "block";
+  img.style.display = "block";
+
+  setTimeout(() => {
+    img.style.opacity = "1";
+  }, 50);
+}
