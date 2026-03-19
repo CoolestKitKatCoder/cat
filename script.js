4b4737f288e3532dc8b3da5ba905fd229e32f7c6
CoolestKitKatCoder <tinycodingcatmeow@proton.me>
2026-03-19 07:32:40 +0400

    Update script.js

--- script.js
+++ script.js
@@ -3,20 +3,13 @@ function showCat() {
   let img = document.getElementById("catImg");
   let btn = document.querySelector("button");
 
-  // show elements
   text.style.display = "block";
   img.style.display = "block";
 
-  // trigger animations
   setTimeout(() => {
     text.classList.add("show");
     img.classList.add("show");
   }, 50);
 
-  // button bounce
   btn.classList.add("clicked");
-  setTimeout(() => {
-    btn.classList.remove("clicked");
-  }, 400);
 }
-
